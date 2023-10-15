ce15cc72769237691892ceba6829c04559b88d41
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-15 18:42:00 +0530

    looping out tasks

--- src/component/task/taskComponent.jsx
+++ src/component/task/taskComponent.jsx
@@ -2,17 +2,30 @@ import TaskAction from "../taskAction/taskActionComponent";
 import TaskContent from "../taskContent/taskContentComponent";
 
 const Task = () => {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  console.log("existing Task", tasks);
+
   return (
     <>
-      <div className="mt-3 p-5 flex justify-between items-center bg-lightGray rounded-lg w-full">
-        <div className="bg-white rounded-sm w-full flex items-center">
-          <div className="flex justify-between items-center w-full">
-            {/* task-section */}
-            <TaskContent />
-            {/* action-section */}
-            <TaskAction />
-          </div>
-        </div>
+      <div className="mt-3 p-5 flex flex-col gap-3 justify-between items-center bg-lightGray rounded-lg w-full">
+        {tasks.map((task) => {
+          // destructure the array
+          const { id, title, status } = task;
+          return (
+            <div
+              className="bg-white rounded-[5px] w-full flex items-center"
+              key={id}
+            >
+              <div className="flex justify-between items-center w-full">
+                {/* task-section */}
+                <TaskContent title={title} status={status} />
+                {/* action-section */}
+                <TaskAction />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
