7e632f0ef55f382eb2708e26adfab7027db54f5c
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-15 18:32:22 +0530

    model close function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BiSolidTrashAlt, BiSolidPencil } from "react-icons/bi";
 import "./App.css";
 
 const App = () => {
+  // model window state
   const [isModelOpen, setIsModelOpen] = useState(false);
 
   const modelOpen = () => {
