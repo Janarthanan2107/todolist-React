9f41fe35925cb3ad91ef80962e6a07e1a3aecd06
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-16 13:05:50 +0530

    merging both content

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ModalProvider } from './context/modal.context.jsx'
+import { TaskProvider } from './context/task.context.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TaskProvider>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </TaskProvider>
   </React.StrictMode>,
 )
