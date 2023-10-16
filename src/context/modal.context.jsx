956b94e0cd9a322ed6fdcce01fb4101003cb7d61
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-16 13:04:40 +0530

    adding model context

---
+++ src/context/modal.context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const ModalContext = createContext();
+
+const ModalProvider = ({children})=>{
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = ()=>{
+        setIsModalOpen(true)
+    }
+
+    const closeModal = ()=>{
+        setIsModalOpen(false)
+    }
+    const initialValues = {
+        openModal, closeModal, isModalOpen
+    }
+
+    return (<ModalContext.Provider value={initialValues}>
+        {children}
+    </ModalContext.Provider>)
+}
+
+//custom hook
+const useModalContext = ()=>{
+    return useContext(ModalContext)
+}
+
+export {ModalProvider, useModalContext}
