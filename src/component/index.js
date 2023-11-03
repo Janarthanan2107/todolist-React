e2c26bb051dd7518ffeb6c50a6247e3a9abe7ff7
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-11-04 01:50:33 +0530

    Update index.js

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,7 @@
+// import all the component to index 
 import Controls from "./controls/controlsComponent";
 import Task from "./task/taskComponent";
 import Model from "./modelWindow/model";
+
+// export all the components for using on another components ex: App.js
 export { Task, Model, Controls }
